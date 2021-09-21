5aad216182a66e14f41b88f59687de2c962f838d
VanessaCarvalho-VNS <vnscarvalh@gmail.com>
2021-09-21 20:06:33 -0300

    Comecando a implementar um metodo que imprime o tabuleiro

--- Xadrez-Console/Program.cs
+++ Xadrez-Console/Program.cs
@@ -8,6 +8,8 @@ namespace Xadrez_Console {
 
             Tabuleiro tab = new Tabuleiro(8, 8);
 
+            Tela.imprimirTabuleiro(tab);
+
             Console.ReadLine();
         
             
--- Xadrez-Console/Tabuleiro/Posicao.cs
+++ Xadrez-Console/Tabuleiro/Posicao.cs
@@ -7,17 +7,12 @@
         public Posicao(int linha, int coluna) {
             this.linha = linha;
             this.coluna = coluna;
-
-        
-
         }
 
         public override string ToString() {
-
             return linha
                 + ", "
                 + coluna;
         }
-
     }
 }
