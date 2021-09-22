e07e5433673d8fa057312e89115cb434ca6ee54c
VanessaCarvalho-VNS <vnscarvalh@gmail.com>
2021-09-22 17:33:38 -0300

    Fazendo os primeiros testes de movimentacao

--- Xadrez-Console/Tabuleiro/Peca.cs
+++ Xadrez-Console/Tabuleiro/Peca.cs
@@ -14,6 +14,12 @@
         
         }
 
+        public void incrementarQteMovimentos() {
+            qteMovimentos++;
+        
+
+        }
+
     }
 }   
         
--- Xadrez-Console/Tabuleiro/Tabuleiro.cs
+++ Xadrez-Console/Tabuleiro/Tabuleiro.cs
@@ -28,11 +28,23 @@
         public void colocarPeca(Peca p, Posicao pos) {
             if (existePeca(pos)) {
                 throw new TabuleiroException("Já existe uma peça nessa posição!");
-            
-
             }
             pecas[pos.linha, pos.coluna] = p;
             p.posicao = pos;
+        }
+
+        public Peca retirarPeca(Posicao pos) { 
+            if (peca(pos) == null) {
+                return null;
+            
+
+            }
+            Peca aux = peca(pos);
+            aux.posicao = null;
+            pecas[pos.linha, pos.coluna] = null;
+            return aux;
+        
+
         }
 
         public bool posicaoValida(Posicao pos) { 
